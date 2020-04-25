bd8c6272ad77fb28016c8d05af9b5b6829894207
Nursultan <nursultan.sharsheev@gmail.com>
2020-04-25 20:13:37 +0600

    bg_changed

--- src/components/MainFetcher/MainInfo/MainInfo.js
+++ src/components/MainFetcher/MainInfo/MainInfo.js
@@ -4,18 +4,15 @@ const MainInfo = (props) => {
 
     return (
         <div className='col-md-3'>
+            <div className='infoPlaces'>
+                <span className='title-char'>Name: </span><span>{props.state.name}</span>
+            </div>
             <div className='d-flex'>
                 <div className='margin0auto'>
                     <img className='imgSize' src={props.state.image} alt=' '/>
                 </div>
             </div>
             <div>
-                <div className='infoPlaces'>
-                    <span className='title-char'>Name: </span><span>{props.state.name}</span>
-                </div>
-                <div className='infoPlaces'>
-                    <span className='title-char'>ID in base: </span><span>{props.state.id}</span>
-                </div>
             </div>
         </div>
 
