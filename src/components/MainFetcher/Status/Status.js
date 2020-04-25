2b52f8c8763ee2f7c6c1816cb0274b1a574ed8f1
Nursultan <nursultan.sharsheev@gmail.com>
2020-04-25 19:29:55 +0600

    bg_changed

--- src/components/MainFetcher/Status/Status.js
+++ src/components/MainFetcher/Status/Status.js
@@ -1,28 +1,22 @@
 import React from 'react';
 
 const Status = (props) => {
-
     let state = props.state;
-
     return (
         <div>
             <div>
-                <p>
+                <p className='m-0'>
                     Status: {state.connection}
                 </p>
-                <p>
+                <p className='m-0'>
                     Items found: {state.length}
                 </p>
-                <p>
+                <p className='m-0'>
                     Item №: {state.itemHero + 1}
                 </p>
-
             </div>
         </div>
-
     )
-
-
 };
 
 export default Status;
